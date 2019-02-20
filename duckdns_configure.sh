8a83788be2873310247184522feff1f5fe3ca32d
Simone Foschi <s.foschi@gmail.com>
2019-02-20 19:28:50 +0100

    Substitution of echo with read in variable assignment

--- duckdns_configure.sh
+++ duckdns_configure.sh
@@ -7,8 +7,8 @@ fi
 
 echo "Before starting the procedure, ensure you have a duckdns.org domain."
 echo "The procedure requires the token of your duckdns account to configure correctly the domain."
-echo -p "Insert the domain name eg.: mydomain.duckdns.org and press [enter]" DOMAIN
-echo -p "Insert the token of your account and press [enter]" TOKEN
+read -p "Insert the domain name eg.: mydomain.duckdns.org and press [enter]" DOMAIN
+read -p "Insert the token of your account and press [enter]" TOKEN
 echo "This procedure will install and configure all software for a working duckdns.org DDNS domain on a Raspbian"
 echo "Starting procedure.."
 apt get install curl
